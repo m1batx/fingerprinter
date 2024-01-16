c9ba3c61b87b6993abb315279f008cf43ee1683b
MBekhit <mohammed.bekhiit@gmail.com>
2024-01-16 22:30:57 +0300

    init

---
+++ nonshwn.py
@@ -0,0 +1,41 @@
+from flask import Flask, request
+import json
+import threading
+
+app = Flask(__name__)
+
+info = {
+    "Machines": []
+}
+
+class DataUser:
+    def __init__(self, ip, agent):
+        self.ip = ip
+        self.agent = agent
+
+# Collect information about the client's computer and system
+@app.route('/')
+def stoleInformation():
+    ip_addr = request.remote_addr
+    user_agent = request.user_agent.string
+    info['Machines'].append(DataUser(ip_addr, user_agent).__dict__)
+    write(info, 'infoMachines.json')
+    return 'We are here'
+
+# Write information about clients to a local disk (accessible to the script creator)
+def write(data, filename):
+    data = json.dumps(data, indent=4)
+    with open(filename, 'w') as f:
+        f.write(data)
+
+def run_flask():
+    app.run(host="0.0.0.0", port=5001)
+
+if __name__ == '__main__':
+    # Start Flask in a separate thread
+    flask_thread = threading.Thread(target=run_flask)
+    flask_thread.daemon = True
+    flask_thread.start()
+
+    # Main program can continue or terminate while Flask runs in the background
+    input("Press Enter to exit...")
---
+++ try1.py
@@ -0,0 +1,31 @@
+from flask import Flask, request
+import json
+app = Flask(__name__)
+
+info = {
+    "Machines": []
+}
+
+class Data_user:
+    def __init__(self, ip, agent):
+        self.ip = ip
+        self.agent = agent
+
+# Cбор информации о клиентском компьютере и системе
+@app.route('/')
+def stoleInfomation():
+    ip_addr = request.remote_addr
+    user_agent = request.user_agent.string
+    info['Machines'].append(Data_user(ip_addr, user_agent).__dict__)
+    write(info, 'infoMachines.json')
+    return 'We are stealing your information'
+
+# Запись информации о клиентах на локальный сетевой диск (доступен создателю скрипта)
+def write(data, filename):
+    data = json.dumps(data)
+    data = json.loads(str(data))
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5001)
